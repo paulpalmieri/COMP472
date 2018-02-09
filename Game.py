efe5d05b36dc25c3bcad82c9ff707d2873ea2ffa
Palmirouze <Palmirouze@gmail.com>
2018-02-09 16:25:54 -0500

    Game now keeps track of possible moves, all moves are indicated as letters

--- Game.py
+++ Game.py
@@ -14,11 +14,14 @@ class Game:
         # holds the letter identifier of the empty tile
         self.empty_row, self.empty_col, self.empty_tile = self.init_empty_tile()
         self.move_list = []
+        self.possible_moves = self.get_legal_moves()
 
     # creates a 2d array and fills the board
     def init_game_board(self, initial_config):
         board = [[None] * Game.cols for _ in range(Game.rows)]
         k = 0
+
+        # iterate 3*5 times and fills each cell with the passed spaced string
         for i, j in product(range(Game.rows), range(Game.cols)):
             board[i][j] = initial_config[k]
             k += 1
@@ -32,24 +35,27 @@ class Game:
 
     # attempts to make a move
     def move(self, tile_letter):
+        if tile_letter in self.possible_moves:
             # get row and col for the tile that needs to move
             tile_row = letter_to_index_map[tile_letter][0]
             tile_col = letter_to_index_map[tile_letter][1]
 
             # swap with empty tile
-            self.swap(tile_row, tile_col, self.empty_row, self.empty_col)
+            self.game_grid[self.empty_row][self.empty_col] = self.game_grid[tile_row][tile_col]
+            self.game_grid[tile_row][tile_col] = 'e'
 
             # update empty tile
             self.empty_row = tile_row
             self.empty_col = tile_col
             self.empty_tile = tile_letter
 
+            # add move
             self.move_list.append(tile_letter)
 
-    # swaps two tiles
-    def swap(self, tile_row, tile_col, empty_row, empty_col):
-        self.game_grid[empty_row][empty_col] = self.game_grid[tile_row][tile_col]
-        self.game_grid[tile_row][tile_col] = 'e'
+            # update legal moves
+            self.possible_moves = self.get_legal_moves()
+
+
 
     # checks for symmetry on first and last row
     def goal_state_reached(self):
@@ -63,29 +69,25 @@ class Game:
         time_stamp = time.strftime("%Y-%m-%d-%H:%M:%S")
         file_name = OUTPUT_PATH + "cc_" + time_stamp + ".txt"
         with open(file_name, 'w') as f:
-            f.write("moves:\n")
-            f.write("".join(self.move_list))
+            f.write("moves:\t" + "".join(self.move_list))
 
-    # get legal moves for this game at this state
-    def get_complete_adjacent_tiles(self):
+    def get_legal_moves(self):
         adj_list = []
+
+        # check result of potential moves
         up = self.empty_row - 1 >= 0
         down = self.empty_row + 1 <= 2
         left = self.empty_col - 1 >= 0
         right = self.empty_col + 1 <= 4
 
         if up:
-            node = [self.empty_row - 1, self.empty_col, 'UP', letter_list[self.empty_row - 1][self.empty_col]]
-            adj_list.append(node)
+            adj_list.append(letter_list[self.empty_row - 1][self.empty_col])
         if down:
-            node = [self.empty_row + 1, self.empty_col, 'DOWN', letter_list[self.empty_row + 1][self.empty_col]]
-            adj_list.append(node)
+            adj_list.append(letter_list[self.empty_row + 1][self.empty_col])
         if left:
-            node = [self.empty_row, self.empty_col - 1, 'LEFT', letter_list[self.empty_row][self.empty_col - 1]]
-            adj_list.append(node)
+            adj_list.append(letter_list[self.empty_row][self.empty_col - 1])
         if right:
-            node = [self.empty_row, self.empty_col + 1, 'RIGHT', letter_list[self.empty_row][self.empty_col + 1]]
-            adj_list.append(node)
+            adj_list.append(letter_list[self.empty_row][self.empty_col + 1])
 
         return adj_list
 
