d0dbe07177a56e0a393f950d494999a713665980
friesenA <amanda.friesen@gmail.com>
2018-03-28 12:34:34 -0400

    new test cases

--- fileCreator.py
+++ fileCreator.py
@@ -1,8 +1,8 @@
 from random import shuffle
 
-initial = 'rrrrbbbbyywwgge'
+initial = 'rrrrbbwwyyggppe'
 initiallist = list(initial)
-with open("input/expert.txt", 'w') as f:
-    for i in range(0, 100):
+with open("input/master1000.txt", 'w') as f:
+    for i in range(0, 1000):
         shuffle(initiallist)
         f.write(" ".join(initiallist) + '\n')
